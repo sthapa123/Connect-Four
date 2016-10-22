761681808f48fba39c13a2156290ea762e8927cc
14coruma <14coruma@gmail.com>
2016-10-22 13:58:55 -0400

    Initial commit

---
+++ connectfourTests/ProgramTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using connectfour;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace connectfour.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void checkForVerticalRowsTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
